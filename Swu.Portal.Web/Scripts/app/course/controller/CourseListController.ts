4c85f09a636fafce46969f6b95ef59d82c332da4
Chansak Kochasen <chansak.kochasen@allianz.com>
2017-08-25 11:44:08 +0700

    Hiligh current page

--- Swu.Portal.Web/Scripts/app/course/controller/CourseListController.ts
+++ Swu.Portal.Web/Scripts/app/course/controller/CourseListController.ts
@@ -1,5 +1,11 @@
 ﻿module Swu {
-    export interface ICourseListScope extends ng.IScope {
+    export interface IPagination extends ng.IScope {
+        paginate<T>(data: T[], displayData: T[], pageSize: number, currentPage: number): void;
+        next(): void;
+        prev(): void;
+        changePage(page: number): void;
+    }
+    export interface ICourseListScope extends IPagination{
         currentPage: number;
         pageSize: number;
         totalPageNumber: number;
@@ -8,8 +14,6 @@
         displayCourses: ICourseBriefDetail[];
         getCourseByCriteria(criteria: CourseCritirea): void;
         getTotalPageNumber(): number;
-        paginate(courses: ICourseBriefDetail[], pageSize: number, currentPage: number): void;
-        changePage(page: number): void;
         search(): void;
     }
     @Module("app")
@@ -22,7 +26,7 @@
                     this.$scope.courses = response;
                     this.$scope.totalPageNumber = this.$scope.getTotalPageNumber();
                     //this.$scope.currentPage = 2;
-                    this.$scope.paginate(this.$scope.courses, this.$scope.pageSize, this.$scope.currentPage);
+                    this.$scope.paginate<ICourseBriefDetail>(this.$scope.courses,this.$scope.displayCourses, this.$scope.pageSize, this.$scope.currentPage);
                 }, (error) => { });
             };
             this.$scope.getTotalPageNumber = (): number => {
@@ -32,15 +36,31 @@
                 console.log(this.$scope.criteria);
                 this.$scope.getCourseByCriteria(this.$scope.criteria);
             };
-            this.$scope.paginate = (courses: ICourseBriefDetail[], pageSize: number, currentPage: number) => {
-                console.log(this.$scope.courses.length);
-                this.$scope.displayCourses = courses.slice(currentPage * pageSize, (currentPage + 1) * pageSize);
-                console.log(this.$scope.displayCourses.length);
+
+            //Pagination section
+            this.$scope.paginate = (data: ICourseBriefDetail[], displayData: ICourseBriefDetail[], pageSize: number, currentPage: number) => {
+                displayData = data.slice(currentPage * pageSize, (currentPage + 1) * pageSize);
+                this.$scope.displayCourses = displayData;
             };
             this.$scope.changePage = (page: number) => {
                 this.$scope.currentPage = page;
-                this.$scope.paginate(this.$scope.courses, this.$scope.pageSize, this.$scope.currentPage);
+                console.log('current page: '+page);
+                this.$scope.paginate<ICourseBriefDetail>(this.$scope.courses,this.$scope.displayCourses, this.$scope.pageSize, this.$scope.currentPage);
             };
+            this.$scope.next = () => {
+                var nextPage = this.$scope.currentPage + 1;
+                if (nextPage < this.$scope.getTotalPageNumber()){
+                    this.$scope.changePage(nextPage);
+                }
+            };
+            this.$scope.prev = () => {
+                var prevPage = this.$scope.currentPage - 1;
+                if (prevPage >= 0) {
+                    this.$scope.changePage(prevPage);
+                }
+            };
+            //End Pagination section
+            
             this.init();
         }
 
