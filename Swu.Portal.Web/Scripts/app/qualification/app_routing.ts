9d48b657bfb74d0353b3df925b3749c8506f1e91
Chansak Kochasen <chansak.kochasen@allianz.com>
2018-02-06 14:43:19 +0700

    Update committee for  Nottingham

---
+++ Swu.Portal.Web/Scripts/app/qualification/app_routing.ts
@@ -0,0 +1,19 @@
+﻿module Swu {
+    @Module("app")
+    @Config
+    class StateConfig {
+        static $inject: Array<string> = ["$stateProvider", "$urlRouterProvider", "$locationProvider", "$httpProvider"];
+        constructor(
+            private $stateProvider: ng.ui.IStateProvider,
+            private $urlRouterProvider: ng.ui.IUrlRouterProvider,
+            private $locationProvider: ng.ILocationProvider,
+            private $httpProvider: ng.IHttpProvider) {
+            $stateProvider
+                .state("qualification", {
+                    url: "/qualification",
+                    templateUrl: "/Scripts/app/qualification/view/qualification.html",
+                    controller: "QualificationController as vm"
+                });
+        }
+    }
+} 
